14965c4c4db3bfebffef95dfb09cd4cede2a21f3
PatelMegha1 <meghapatel1903@gmail.com>
2020-03-06 13:10:44 -0500

    final

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('home.ejs', { title: 'Megha' });
 });
 
+router.get('/test', function(req, res, next) {
+  res.render('test.ejs', { title: 'Megha' });
+});
+
+
 module.exports = router;
